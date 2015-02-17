e46e89da2ba662ab09bab612f51e07ee983b2cc9
dyep49 <dyep49@gmail.com>
2015-02-17 15:07:28 -0500

    adds isotope filtering

--- src/scripts/isotope.js
+++ src/scripts/isotope.js
@@ -2,19 +2,34 @@ var Isotope = require('isotope-layout');
 var imagesLoaded = require('imagesloaded');
 
 module.exports = function() {
+    var iso;
 
     var container = document.querySelector('#isotope-container')
     //init
     imagesLoaded(container, function() {
-      var iso = new Isotope(container, {
+      iso = new Isotope(container, {
         itemSelector: '.project-item',
         masonry: {
-          columnWidth: 270,
+          columnWidth: 360,
           isFitWidth: true
         }
       });      
-    })
+    });
 
+    var filters = document.querySelectorAll('.filter-container li')
+    var filterLength = filters.length;
+
+    for(var i=0; i < filterLength; i++) {
+        var currentFilter = filters[i];
+        currentFilter.onclick = function() {
+            iso.arrange({
+                filter: function(itemElem) {
+                    var random = Math.random();
+                    return random > 0.5;
+                }
+            })
+        }        
+    }
 
 
     // // filter functionality
